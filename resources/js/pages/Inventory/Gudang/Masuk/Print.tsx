11ad6044297c2c066a7c4bd3f652aa7333e3dfea
Daniel <daniel.gulla88@gmail.com>
2025-10-29 18:11:03 +0800

    add print gudang masuk

--- resources/js/pages/Inventory/Gudang/Masuk/Print.tsx
+++ resources/js/pages/Inventory/Gudang/Masuk/Print.tsx
@@ -1,25 +1,100 @@
 import React from "react";
 import { GudangMasuk } from "../../models";
+import { formatTgl } from "@/pages/components/functions";
+import { formatDigit } from "@/pages/components/helpers";
 
 interface Props {
     data: GudangMasuk;
-    canWrite: boolean;
 }
 
-const Print: React.FC<Props> = ({ data, canWrite }) => {
+const Print: React.FC<Props> = ({ data }) => {
+    const supplier = data.supplier;
+    const isLunas = data.is_lunas;
+
     return (
-        <div>
-            <h1>Print Gudang Masuk</h1>
-            <p>Penerima: {data.penerima}</p>
-            <p>Supplier: {data.supplier?.nama}</p>
-            <h2>Details:</h2>
-            <ul>
-                {data.details?.map((v) => (
-                    <li key={v.id}>
-                        {v.barang?.deskripsi} - {v.qty} {v.barang?.satuan}
-                    </li>
-                ))}
-            </ul>
+        <div className="w-full h-screen bg-white px-4 py-2 mx-auto text-xs text-black">
+
+            <div className="w-full flex flex-col items-center justify-center mt-4">
+                <div className="text-xl font-semibold py-2 underline">TANDA TERIMA BARANG</div>
+            </div>
+
+            <div className="w-full flex flex-row items-center justify-center mt-4">
+
+                <div className="w-full flex flex-col items-center justify-center">
+                    <div className="text-xl font-semibold">{supplier?.nama}</div>
+                    <div className="text-sm">{supplier?.alamat}</div>
+                </div>
+
+                <div className="w-full flex flex-row justify-center gap-1 text-sm">
+                    <div className="flex flex-col text-right min-w-40">
+                        <div className="mb-0.5 font-semibold border border-gray-400 px-2 py-0.5 rounded-sm">Tanggal Terima</div>
+                        <div className="mb-0.5 font-semibold border border-gray-400 px-2 py-0.5 rounded-sm">Penerima</div>
+                        <div className="mb-0.5 font-semibold border border-gray-400 px-2 py-0.5 rounded-sm">Jatuh Tempo</div>
+                        <div className="mb-0.5 font-semibold border border-gray-400 px-2 py-0.5 rounded-sm">Status</div>
+                        {isLunas && <div className="mb-0.5 font-semibold border border-gray-400 px-2 py-0.5 rounded-sm">Tanggal Pelunasan</div>}
+                    </div>
+                    <div className="flex flex-col">
+                        <div className="mb-0.5 font-semibold px-2 py-0.5">:</div>
+                        <div className="mb-0.5 font-semibold px-2 py-0.5">:</div>
+                        <div className="mb-0.5 font-semibold px-2 py-0.5">:</div>
+                        <div className="mb-0.5 font-semibold px-2 py-0.5">:</div>
+                        {isLunas && <div className="mb-0.5 font-semibold px-2 py-0.5">:</div>}
+                    </div>
+                    <div className="flex flex-col min-w-32">
+                        <div className="mb-0.5 border border-gray-400 px-2 py-0.5 rounded-sm">{formatTgl(data.tgl)}</div>
+                        <div className="mb-0.5 border border-gray-400 px-2 py-0.5 rounded-sm">{data.penerima}</div>
+                        <div className="mb-0.5 border border-gray-400 px-2 py-0.5 rounded-sm">{formatTgl(data.due)}</div>
+                        <div className="mb-0.5 border border-gray-400 px-2 py-0.5 rounded-sm">{isLunas ? 'Lunas' : 'Belum Lunas'}</div>
+                        {isLunas && <div className="mb-0.5 border border-gray-400 px-2 py-0.5 rounded-sm">{formatTgl(data.tgl_lunas!)}</div>}
+                    </div>
+                </div>
+            </div>
+
+            <div className="w-full overflow-x-auto h-full text-sm mt-4">
+                <table className="w-full border-collapse border border-black">
+                    <thead>
+                        <tr className="bg-gray-300">
+                            <th className="border border-black p-3">No</th>
+                            <th className="border border-black p-3">Deskripsi Barang</th>
+                            <th className="border border-black p-3">Harga</th>
+                            <th className="border border-black p-3">Qty</th>
+                            <th className="border border-black p-3">Jumlah</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.details && data.details.map((detail, index) => (
+                            <tr key={detail.id}>
+                                <td className="border border-black px-2 py-2 text-center w-12">{index + 1}</td>
+                                <td className="border border-black px-2 py-2">{detail.barang?.deskripsi}</td>
+                                <td className="border border-black px-2 py-2 text-center">{formatDigit(detail.harga, 2)}</td>
+                                <td className="border border-black px-2 py-2 text-center">{formatDigit(detail.qty, 2)} {detail.barang?.satuan}</td>
+                                <td className="border border-black px-2 py-2 text-center">{formatDigit(detail.total, 2)}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+
+                    <tfoot>
+                        <tr className="text-lg font-bold">
+                            <td colSpan={4} className="border border-black p-3 text-right">Total Bayar Rp.</td>
+                            <td className="border border-black p-2 text-center">{formatDigit(data.total, 2)}</td>
+                        </tr>
+                    </tfoot>
+                </table>
+
+                <div className="w-full flex flex-row justify-around items-start gap-1 text-sm mt-8">
+                    <div className="flex flex-col items-center justify-center mt-8">
+                        <div className="mb-0.5 font-semibold px-2 py-0.5">{data.supplier?.nama}</div>
+                        <div className="mb-0.5 font-semibold px-2 pt-24 border-b border-black w-32"></div>
+                        <div className="mb-0.5 font-semibold px-2 py-1">(Kurir)</div>
+                    </div>
+                    <div className="flex flex-col items-center justify-center mt-8">
+                        <div className="mb-0.5 font-semibold px-2 py-0.5">Yang Menerima,</div>
+                            <div className="mb-0.5 font-semibold px-2 pt-16">{data.penerima}</div>
+                        <div className="mb-0.5 font-semibold px-2 py-1 border-b border-black w-32"></div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
